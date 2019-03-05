c52061f1b0ce385206b7be0cc7a7924e5bfffbfe
felex <felicxmose@msn.com>
2019-03-05 11:34:48 +0100

    startet på opgave3

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,20 @@
 //Her skal du skrive din typescriptkode
+let li0 = document.getElementById('0');
+console.log(li0);
+
+let list = document.getElementsByClassName('sundt');
+console.log(list);
+
+let tagNameUl = document.getElementsByTagName('ul');
+console.log(tagNameUl);
+
+let getLiTags = document.getElementsByTagName('li');
+
+for (var val in getLiTags){
+    console.log(val);
+}
+
+//
+let id2Tag = document.getElementById('2');
+id2Tag.setAttribute('class','sundt');
+
